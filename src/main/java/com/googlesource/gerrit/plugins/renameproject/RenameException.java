4c4a36b3e76e8990201a26bbc4b8b9f2d6095b35
Nguyen Tuan Khang Phan <nguyen.tuan.khang.phan@ericsson.com>
2019-09-24 13:01:46 -0400

    Add error handling if rename was not successful
    Plugin error handling didn't cover the fact that if change-Ids updates
    fail it will not revert previous steps, meaning changes will be linked
    to a non existent project.
    
    This change adds error handling that performs revert if rename
    procedure was not successful.
    
    Add RenameExcpetion to handle chained exception that will help to keep
    track of excptions that occured both during rename procedure and its
    revert.
    
    Fix error handling within DatabaseRenameHandler:
    ReviewDb part:
    Add missing watched project update during revert. Refactor
    renameInReviewDb to set AutoCommit to true only if rename succeeded or
    after rollback was called. Previously, AutoCommit was set to true before
    rollback, making rollback fail.
    
    Error handling is hard to test automatically due to how it was
    implemented (most of the handling methods are private and mocking some
    components might end up in testing mocking itself). However, manual
    testing revealed shortcomings explained above.
    
    Manual test was done by manually throwing exceptions was done in for
    classes:
    - RenameProject: all steps of doRename were tested with reviewDb and
      noteDb.
    - DatabaseRenameHandler: tested rollback on partial changeId updates
      for both reviewDb and noteDb.
    
    Bug: Issue 11734
    Change-Id: I252b7071b5575241a7aba1c3d3537b4775c024a2

---
+++ src/main/java/com/googlesource/gerrit/plugins/renameproject/RenameException.java
@@ -0,0 +1,7 @@
+package com.googlesource.gerrit.plugins.renameproject;
+
+public class RenameException extends Exception{
+  public RenameException(String message, Throwable cause){
+    super(message, cause);
+  }
+}
