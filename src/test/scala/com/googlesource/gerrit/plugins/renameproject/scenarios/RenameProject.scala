078de11520bd2137448b221a54cfebbf707cbecb
Marco Miller <marco.miller@ericsson.com>
2020-09-21 17:04:50 -0400

    e2e-test: Rename 'name' after GerritSimulation core change
    Change-Id: Ibd7bca09177a3f4913fa1e71fe823ffb6a7a1a21

--- src/test/scala/com/googlesource/gerrit/plugins/renameproject/scenarios/RenameProject.scala
+++ src/test/scala/com/googlesource/gerrit/plugins/renameproject/scenarios/RenameProject.scala
@@ -23,7 +23,7 @@ import scala.concurrent.duration._
 
 class RenameProject extends GerritSimulation {
   private val data: FeederBuilder = jsonFile(resource).convert(keys).queue
-  private val default = name
+  private val default = className
   private val renamedTo = unique
 
   override def replaceOverride(in: String): String = {
