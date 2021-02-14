f87329c828922960e8ad0cb5e9fa46a4e40ea766
Nguyen Tuan Khang Phan <nguyen.tuan.khang.phan@ericsson.com>
2021-02-14 06:26:01 -0500

    Introduce rename propagation feature
    With this change rename project can propagate rename-project operation
    to other instances using REST API.
    
    Change-Id: Icbe98eb6af9bffc38f4b882c149b9092f9701f3c

--- src/main/java/com/googlesource/gerrit/plugins/renameproject/Configuration.java
+++ src/main/java/com/googlesource/gerrit/plugins/renameproject/Configuration.java
@@ -14,24 +14,116 @@
 
 package com.googlesource.gerrit.plugins.renameproject;
 
+import com.google.common.base.CharMatcher;
+import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.flogger.FluentLogger;
 import com.google.gerrit.extensions.annotations.PluginName;
 import com.google.gerrit.server.config.PluginConfig;
 import com.google.gerrit.server.config.PluginConfigFactory;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import com.googlesource.gerrit.plugins.renameproject.conditions.RenamePreconditions;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.eclipse.jgit.lib.Config;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Singleton
 public class Configuration {
 
+  private static final Logger log = LoggerFactory.getLogger(Configuration.class);
   private final int indexThreads;
+  private final Http http;
 
   @Inject
   public Configuration(PluginConfigFactory pluginConfigFactory, @PluginName String pluginName) {
     PluginConfig cfg = pluginConfigFactory.getFromGerritConfig(pluginName);
+    Config globalPluginConfig = pluginConfigFactory.getGlobalPluginConfig(pluginName);
     indexThreads = cfg.getInt("indexThreads", 4);
+    http = new Http(globalPluginConfig);
+  }
+
+  private static int getInt(Config cfg, String section, String name, int defaultValue) {
+    try {
+      return cfg.getInt(section, name, defaultValue);
+    } catch (IllegalArgumentException e) {
+      log.error(String.format("invalid value for %s; using default value %d", name, defaultValue));
+      log.warn("Failed to retrieve integer value", e);
+      return defaultValue;
+    }
+  }
+
+  public Http http() {
+    return http;
   }
 
   public int getIndexThreads() {
     return indexThreads;
+  }
+
+  public static class Http {
+
+    static final String USER_KEY = "user";
+    static final String PASSWORD_KEY = "password";
+    static final String CONNECTION_TIMEOUT_KEY = "connectionTimeout";
+    static final String SOCKET_TIMEOUT_KEY = "socketTimeout";
+    static final String MAX_TRIES_KEY = "maxTries";
+    static final String RETRY_INTERVAL_KEY = "retryInterval";
+    static final String HTTP_SECTION = "http";
+    static final String URL_KEY = "url";
+    static final String REPLICA_SECTION = "replicaInfo";
+
+    static final int DEFAULT_TIMEOUT_MS = 5000;
+    static final int DEFAULT_MAX_TRIES = 360;
+    static final int DEFAULT_RETRY_INTERVAL = 10000;
+
+    private final String user;
+    private final String password;
+    private final int connectionTimeout;
+    private final int socketTimeout;
+    private final int maxTries;
+    private final int retryInterval;
+    private final Set<String> urls;
+
+    private Http(Config cfg) {
+      user = Strings.nullToEmpty(cfg.getString(HTTP_SECTION, null, USER_KEY));
+      password = Strings.nullToEmpty(cfg.getString(HTTP_SECTION, null, PASSWORD_KEY));
+      connectionTimeout = getInt(cfg, HTTP_SECTION, CONNECTION_TIMEOUT_KEY, DEFAULT_TIMEOUT_MS);
+      socketTimeout = getInt(cfg, HTTP_SECTION, SOCKET_TIMEOUT_KEY, DEFAULT_TIMEOUT_MS);
+      maxTries = getInt(cfg, HTTP_SECTION, MAX_TRIES_KEY, DEFAULT_MAX_TRIES);
+      retryInterval = getInt(cfg, HTTP_SECTION, RETRY_INTERVAL_KEY, DEFAULT_RETRY_INTERVAL);
+
+      urls =
+          Arrays.stream(cfg.getStringList(REPLICA_SECTION, null, URL_KEY))
+              .filter(Objects::nonNull)
+              .filter(s -> !s.isEmpty())
+              .map(s -> CharMatcher.is('/').trimTrailingFrom(s))
+              .collect(Collectors.toSet());
+      log.warn("Urls: " + urls);
+    }
+
+    public Set<String> urls() {
+      return ImmutableSet.copyOf(urls);
+    }
+
+    public String user() {
+      return user;
+    }
+
+    public String password() {
+      return password;
+    }
+
+    public int connectionTimeout() {
+      return connectionTimeout;
+    }
+
+    public int socketTimeout() {
+      return socketTimeout;
+    }
   }
 }
