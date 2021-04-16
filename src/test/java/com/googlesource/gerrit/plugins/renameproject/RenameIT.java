85a77947bf8051ea200cd8566f944f97749081ba
Mark Bekhet <mark.bekhet@ericsson.com>
2021-04-16 13:29:29 -0400

    Introduce an optional configuration to define a regex for naming rules
    Before this change, the plugin made checks on the name to verify that
    the new project name is not null and that it doesn't exist. Therefore,
    the new project name can have a name with any set of characters while
    complying to the naming rules. However, due to IRI (not URI) and
    backward compatibility, the non-ASCII characters may be represented in a
    way that can cause issues [1].
    
    This change introduces a configurable parameter that can be set to
    define a regex that is then enforced upon renaming a project to ensure
    backward compatibility.
    
    [1] https://en.wikipedia.org/wiki/Internationalized_Resource_Identifier
    
    Bug: Issue 14297
    Change-Id: Ia049cb399afe1091626cafebfb3d64f72f41e61f

--- src/test/java/com/googlesource/gerrit/plugins/renameproject/RenameIT.java
+++ src/test/java/com/googlesource/gerrit/plugins/renameproject/RenameIT.java
@@ -57,6 +57,7 @@ public class RenameIT extends LightweightPluginDaemonTest {
   private static final String CACHE_NAME = "changeid_project";
   private static final String REPLICATION_OPTION = "--replication";
   private static final String URL = "ssh://localhost:29418";
+  private static final String RENAME_REGEX = "[a-zA-Z]+";
 
   @Inject private RequestScopeOperations requestScopeOperations;
 
@@ -251,6 +252,32 @@ public class RenameIT extends LightweightPluginDaemonTest {
 
     ProjectState projectState = projectCache.get(Project.nameKey(newProjectName));
     assertThat(projectState).isNull();
+  }
+
+  @Test
+  @UseLocalDisk
+  @GerritConfig(name = "plugin.rename-project.renameRegex", value = RENAME_REGEX)
+  public void testRenameViaHttpWithNonMatchingNameFail() throws Exception {
+    createChange();
+    RestResponse r = renameProjectTo(NEW_PROJECT_NAME + "1");
+    r.assertBadRequest();
+
+    ProjectState projectState = projectCache.get(Project.nameKey(NEW_PROJECT_NAME));
+    assertThat(projectState).isNull();
+  }
+
+  @Test
+  @UseLocalDisk
+  @GerritConfig(name = "plugin.rename-project.renameRegex", value = RENAME_REGEX)
+  public void testRenameViaHttpWithMatchingNameSuccess() throws Exception {
+    createChange();
+    RestResponse r = renameProjectTo(NEW_PROJECT_NAME);
+    r.assertOK();
+
+    ProjectState projectState = projectCache.get(Project.nameKey(NEW_PROJECT_NAME));
+    assertThat(projectState).isNotNull();
+    assertThat(queryProvider.get().byProject(project)).isEmpty();
+    assertThat(queryProvider.get().byProject(Project.nameKey(NEW_PROJECT_NAME))).isNotEmpty();
   }
 
   private RestResponse renameProjectTo(String newName) throws Exception {
