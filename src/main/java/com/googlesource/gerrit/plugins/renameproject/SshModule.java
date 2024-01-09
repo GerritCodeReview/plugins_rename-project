1ca7f53f8e55bca0167ec7967c93408241959cf6
Thomas Dräbing <thomas.draebing@sap.com>
2024-01-09 13:05:49 +0100

    Enable MutableGuiceModule errorprone pattern and fix/suppress issues
    See https://errorprone.info/bugpattern/MutableGuiceModule
    
    Change-Id: Ib3b10b53fbb1bb06260baadfef86ca57ffa166c3

--- src/main/java/com/googlesource/gerrit/plugins/renameproject/SshModule.java
+++ src/main/java/com/googlesource/gerrit/plugins/renameproject/SshModule.java
@@ -14,11 +14,18 @@
 
 package com.googlesource.gerrit.plugins.renameproject;
 
+import com.google.gerrit.extensions.annotations.PluginName;
 import com.google.gerrit.sshd.SingleCommandPluginModule;
 import com.google.inject.binder.LinkedBindingBuilder;
+import com.google.inject.Inject;
 import org.apache.sshd.server.command.Command;
 
 public class SshModule extends SingleCommandPluginModule {
+  @Inject
+  SshModule(@PluginName String pluginName) {
+    super(pluginName);
+  }
+
   @Override
   protected void configure(LinkedBindingBuilder<Command> bind) {
     bind.to(RenameCommand.class);
